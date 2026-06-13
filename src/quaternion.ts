3449ec8799d9c4db1d54b77fd957a3153e49b417
AssoDePicche <samuelprado730@gmail.com>
2026-06-13 16:38:40 -0300

    feat: abstract quaternion

---
+++ src/quaternion.ts
@@ -0,0 +1,8 @@
+export class Quaternion {
+    public constructor(
+        public readonly x: number,
+        public readonly y: number,
+        public readonly z: number,
+        public readonly w: number
+    ) { }
+}
