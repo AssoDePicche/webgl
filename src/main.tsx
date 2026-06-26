b325cb3582d7deab2655c34fdc08b85389393308
AssoDePicche <samuelprado730@gmail.com>
2026-06-26 20:48:07 -0300

    fix: missing dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App';
+import { App } from './App.js';
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')!;
 
 ReactDOM.createRoot(root).render(
     <React.StrictMode>
